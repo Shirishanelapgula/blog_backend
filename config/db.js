147d341898575e523db182f8e63fdbdde4ef5d5b
shirishanelapogula <shirisha@godspeed.systems>
2024-02-02 16:01:17 +0530

    fourth commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shirishanelapogula:6Trl1RcRIQCcSPP2@cluster0.rabrwjy.mongodb.net/blog-app?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb+srv://shirisha:K6KyK4fPJX7Msonm@cluster2.cm7tvff.mongodb.net/blog-backend?retryWrites=true&w=majority");
         console.log("Connected successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
